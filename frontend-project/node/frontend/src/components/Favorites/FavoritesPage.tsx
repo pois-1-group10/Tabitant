b2bb53f6ba0a94c06428bda4d6acf7b7aa2d0632
hrt02k19 <hirotok1435@icloud.com>
2024-05-07 04:13:53 +0900

    add pages to show tankas in list

---
+++ frontend-project/node/frontend/src/components/Favorites/FavoritesPage.tsx
@@ -0,0 +1,38 @@
+/** @jsxImportSource @emotion/react */
+
+import React from "react";
+import { css } from "@emotion/react";
+import BackButton from "../common/BackButton";
+import TankaList from "../common/TankaList";
+
+export default function FavoritesPage() {
+  return (
+    <div css={backgroundStyle}>
+      <div css={buttonWrapperStyle}>
+        <BackButton />
+      </div>
+      <div css={sectionTitleStyle}>いいねした作品</div>
+      <TankaList />
+    </div>
+  )
+}
+
+const backgroundStyle = css`
+  background: linear-gradient(to bottom, #ffffff, #ff981f 50%);
+  padding: 24px;
+  height: calc(100vh - 48px);
+  width: calc(100vw - 48px);
+  overflow: scroll;
+`;
+
+const buttonWrapperStyle = css`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 16px;
+`;
+
+const sectionTitleStyle = css`
+	font-size: 24px;
+	text-align: left;
+`;
