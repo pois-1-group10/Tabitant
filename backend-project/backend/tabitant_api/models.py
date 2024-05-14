9bd31e17410de5f7506187c3b3a9824044260a8f
y-aut <y_autumn0901@icloud.com>
2024-05-14 05:51:03 +0000

    Award, Competition, Prefecture 部分を作成

--- backend-project/backend/tabitant_api/models.py
+++ backend-project/backend/tabitant_api/models.py
@@ -79,3 +79,57 @@ class Competition(models.Model):
 class Award(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='award')
     post=models.ForeignKey(Post, on_delete=models.CASCADE)
+###山本作業分Award
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class Award(models.Model):
+    user_id = models.IntegerField(verbose_name=_("ユーザーID"))
+    tanka_id = models.IntegerField(verbose_name=_("短歌ID"))
+    compe_id = models.IntegerField(verbose_name=_("競技ID"))
+    rank = models.IntegerField(verbose_name=_("順位"))
+
+    class Meta:
+        verbose_name = "受賞"
+        verbose_name_plural = "受賞一覧"
+
+    def __str__(self):
+        return f"Award {self.id}"
+
+
+####山本作業分Competition
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class Competition(models.Model):
+    prefecture_id = models.IntegerField(verbose_name=_("都道府県ID"))
+    year = models.IntegerField(verbose_name=_("年"))
+    month = models.IntegerField(verbose_name=_("月"))
+
+    class Meta:
+        verbose_name = "競技"
+        verbose_name_plural = "競技一覧"
+
+    def __str__(self):
+        return f"Competition {self.id}"
+
+
+####山本作業分Prefecture
+from django.db import models
+
+class Prefecture(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, verbose_name="都道府県")
+
+    class Meta:
+        verbose_name = "都道府県"
+        verbose_name_plural = "都道府県"
+
+
+# AwardからCompetitionへのアクセス
+award = Award.objects.get(pk=1)
+competition = award.compe_id  # AwardオブジェクトからCompetitionオブジェクトへのアクセス
+
+# CompetitionからPrefectureへのアクセス
+competition = Competition.objects.get(pk=1)
+prefecture = competition.prefecture_id  # CompetitionオブジェクトからPrefectureオブジェクトへのアクセス
--- backend-project/backend/tabitant_api/viewsets.py
+++ backend-project/backend/tabitant_api/viewsets.py
@@ -267,3 +267,34 @@ class CommentViewSet(viewsets.ModelViewSet):
         item = self.get_object()
         item.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+#####山本作業分
+# views.py用
+
+from rest_framework import generics
+from .models import Award, Competition, Prefecture
+from .serializers import AwardSerializer, CompetitionSerializer, PrefectureSerializer
+
+class AwardListCreateView(generics.ListCreateAPIView):
+    queryset = Award.objects.all()
+    serializer_class = AwardSerializer
+
+class AwardDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Award.objects.all()
+    serializer_class = AwardSerializer
+
+class CompetitionListCreateView(generics.ListCreateAPIView):
+    queryset = Competition.objects.all()
+    serializer_class = CompetitionSerializer
+
+class CompetitionDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Competition.objects.all()
+    serializer_class = CompetitionSerializer
+
+class PrefectureListCreateView(generics.ListCreateAPIView):
+    queryset = Prefecture.objects.all()
+    serializer_class = PrefectureSerializer
+
+class PrefectureDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Prefecture.objects.all()
+    serializer_class = PrefectureSerializer
