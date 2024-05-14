26b06f8646578bca4f25718ee6262be4e5fa65a3
Koukiymmt <yamamotokouki0106@gmail.com>
2024-05-14 13:32:00 +0900

    Award, Competition, Prefecture 部分を作成

--- backend-project/backend/tabitant_api/serializers.py
+++ backend-project/backend/tabitant_api/serializers.py
@@ -1,1 +1,22 @@
 from rest_framework import serializers
+
+####山本作業分
+# serializers.py用
+
+from rest_framework import serializers
+from .models import Award, Competition, Prefecture
+
+class AwardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Award
+        fields = ['id', 'user_id', 'tanka_id', 'compe_id', 'rank']
+
+class CompetitionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Competition
+        fields = ['id', 'prefecture_id', 'year', 'month']
+
+class PrefectureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Prefecture
+        fields = ['id', 'name']
--- backend-project/backend/tabitant_api/viewsets.py
+++ backend-project/backend/tabitant_api/viewsets.py
@@ -1,1 +1,32 @@
 from rest_framework import viewsets
+
+#####山本作業分
+# views.py用
+
+from rest_framework import generics
+from .models import Award, Competition, Prefecture
+from .serializers import AwardSerializer, CompetitionSerializer, PrefectureSerializer
+
+class AwardListCreateView(generics.ListCreateAPIView):
+    queryset = Award.objects.all()
+    serializer_class = AwardSerializer
+
+class AwardDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Award.objects.all()
+    serializer_class = AwardSerializer
+
+class CompetitionListCreateView(generics.ListCreateAPIView):
+    queryset = Competition.objects.all()
+    serializer_class = CompetitionSerializer
+
+class CompetitionDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Competition.objects.all()
+    serializer_class = CompetitionSerializer
+
+class PrefectureListCreateView(generics.ListCreateAPIView):
+    queryset = Prefecture.objects.all()
+    serializer_class = PrefectureSerializer
+
+class PrefectureDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Prefecture.objects.all()
+    serializer_class = PrefectureSerializer
