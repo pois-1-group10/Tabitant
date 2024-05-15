2e3477e2f1aa93b5be6d99531b015efd156dd256
hrt02k19 <hirotok1435@icloud.com>
2024-05-15 13:05:59 +0000

    add username to required fields of user creation

--- backend-project/backend/tabitant_api/models.py
+++ backend-project/backend/tabitant_api/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
 
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["username"]
 
     class Meta:
         verbose_name = "ユーザー"
