0271419e70226abafed43170f8ce932929ad9cb9
y-aut <y_autumn0901@icloud.com>
2024-05-20 07:18:15 +0000

    frontend: update InfoWindow

--- frontend-project/node/frontend/src/components/Home/Marker.tsx
+++ frontend-project/node/frontend/src/components/Home/Marker.tsx
@@ -1,8 +1,9 @@
 import React, { useCallback, useEffect, useState } from 'react'
-import { MarkerF, InfoWindowF } from '@react-google-maps/api';
+import { MarkerF } from '@react-google-maps/api';
 import { css } from '@emotion/react'
 import { Post } from '../../types/post';
 import { tankaFromPost } from '../../utils/tanka';
+import InfoWindow from './InfoWindow';
 
 export default function Marker({ post }: { post: Post }) {
     const [showDetail, setShowDetail] = useState<boolean>(false);
@@ -21,11 +22,7 @@ export default function Marker({ post }: { post: Post }) {
 
     if (showDetail) {
         return (
-            <InfoWindowF position={new google.maps.LatLng(post.latitude, post.longitude)} onCloseClick={handleInfoWindowCloseClick}>
-                <div>
-                    {tankaFromPost(post)}
-                </div>
-            </InfoWindowF>
+            <InfoWindow post={post} onCloseClick={handleInfoWindowCloseClick} />
         );
     } else {
         return <MarkerF visible={!showDetail} position={new google.maps.LatLng(post.latitude, post.longitude)} onClick={handleMarkerClick} />;
